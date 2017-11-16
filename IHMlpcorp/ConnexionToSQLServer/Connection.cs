7516a2015ae2c4ea6778dc9169084cbc245fccca
rull <julien.rull38@gmail.com>
2017-11-16 15:20:13 +0100

    Validation pour la classe de connection

--- IHMlpcorp/ConnexionToSQLServer/Connection.cs
+++ IHMlpcorp/ConnexionToSQLServer/Connection.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Npgsql;
 namespace ConnectToSQLServer
 {
     public class Connection
     {
+
+        public static NpgsqlConnection Connect(string adresse, string userId, string password, string name)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server="+ adresse + ";User Id="+ userId + ";" +
+                                "Password="+ password + ";Database="+ name +";");
+
+            return conn;
+        }
+
+        public static void TestConnection(string adresse, string userId, string password, string name)
+        {
+            NpgsqlConnection conn = Connect("localhost", "julien", "zone51@", "lpcorp");
+            conn.Open();
+            // Define a query
+            NpgsqlCommand cmd = new NpgsqlCommand("select city from cities", conn);
+
+            // Execute a query
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            // Read all rows and output the first column in each row
+            while (dr.Read())
+                Console.Write("{0}\n", dr[0]);
+
+            // Close connection
+            conn.Close();
+        }
 
     }
 }
--- IHMlpcorp/IHMlpcorp/Form1.Designer.cs
+++ IHMlpcorp/IHMlpcorp/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_test = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_test
             // 
-            this.button1.Location = new System.Drawing.Point(107, 115);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "TEST";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_test.Location = new System.Drawing.Point(107, 115);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "TEST";
+            this.btn_test.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_test);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_test;
     }
 }
 
