2eab959ce75df4951c8013137790b3cc2cedb723
Ramazan <kelleramazan@gmail.com>
2017-11-30 09:22:26 +0100

    Comit pour accepeter suppression

--- IHMlpcorp/IHMlpcorp/Form1.cs
+++ IHMlpcorp/IHMlpcorp/Form1.cs
@@ -24,16 +24,9 @@ namespace IHMlpcorp
 
         private void btn_test_Click(object sender, EventArgs e)
         {
-            //Connection.TestConnection();
-            CsvReader reader = new CsvReader(new string[] { "CODE", "RAISON SOCIALE", "ADRESSE", "CP", "VILLE", "TEL", "FAX", "EMAIL", "ACTIF", "REGLEMENT" });
-            reader.LoadFile("F:\\Julien Rull\\Cours\\PPE\\Negomat_Client2.csv");
-            reader.Read();
-            foreach(string s in reader.GetData()["CODE"])
-            {
-                Console.WriteLine(s);
-            }
 
         }
+
 
         private void button5_Click(object sender, EventArgs e)
         {
